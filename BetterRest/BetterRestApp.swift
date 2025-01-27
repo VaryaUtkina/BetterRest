dd08102e2f1368db5062c8b2af2e9a19712e74e8
Varvara Utkina <varya2412@icloud.com>
2025-01-27 13:16:38 +0300

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Варвара Уткина on 27.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
